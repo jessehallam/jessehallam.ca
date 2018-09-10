becd494b91c3a1899a1317f95c33c5ca8a2e1991
J Hallam <jesse.colin.hallam@gmail.com>
2018-09-10 17:47:14 -0400

    Fixed linting errors.

--- src/server/server.ts
+++ src/server/server.ts
@@ -10,17 +10,17 @@ const app = express()
 app.use(bodyParser.json())
 
 app.post('/api/mail', async (req, res) => {
-    const Mailer = require("node-mailjet").connect(process.env.MAILJET_PUBLIC_KEY, process.env.MAILJET_PRIVATE_KEY);
-    const mailRequest = Mailer.post("send")
+    const Mailer = require('node-mailjet').connect(process.env.MAILJET_PUBLIC_KEY, process.env.MAILJET_PRIVATE_KEY);
+    const mailRequest = Mailer.post('send')
         .request({
-            FromEmail: "contact@jessehallam.ca",
-            FromName: "JesseHallam.ca",
-            Subject: "Contact Form Submission",
+            FromEmail: 'contact@jessehallam.ca',
+            FromName: 'JesseHallam.ca',
+            Subject: 'Contact Form Submission',
             'Text-Part': `Name: ${req.body.name}
 Phone: ${req.body.phone}
 Message: ${req.body.message}`,
             Recipients: [
-                { Email: "jesse.colin.hallam@gmail.com" }
+                { Email: 'jesse.colin.hallam@gmail.com' }
             ]
         });
 
