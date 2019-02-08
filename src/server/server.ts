52228105ea5d97841ada78536a69a2c7b1836f05
J Hallam <jesse.colin.hallam@gmail.com>
2019-02-08 13:31:10 -0500

    Refactoring, code cleanup.

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,7 +3,16 @@ import express from 'express'
 import { HTTP_PORT } from './config'
 import middlewares from './middleware'
 
-const app = express()
+const start = async () => {
+    const app = express()
 
-app.use(middlewares)
-app.listen(HTTP_PORT, () => console.log(`Now listening on port ${HTTP_PORT}`))
+    app.use(middlewares)
+    
+    const server = app.listen(HTTP_PORT, () => {
+        console.log(`Now listening on port ${HTTP_PORT}`)
+    })
+
+    server.on('error', console.error)
+}
+
+start().catch(console.error)
